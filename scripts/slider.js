ddedb85ccf371fc5f70c98bef2ef82638a85b120
Fryderyk Boncler <f.relcnob@gmail.com>
2022-04-26 15:56:39 +0200

    created slider, added desktop styling for header and footer.

---
+++ scripts/slider.js
@@ -0,0 +1,23 @@
+const sliderLeft = document.querySelector(".arrow-left");
+const sliderRight = document.querySelector(".arrow-right");
+let activeSlide = 1;
+
+sliderLeft.addEventListener("click", () => {
+  updateSlide();
+});
+
+sliderRight.addEventListener("click", () => {
+  updateSlide();
+});
+
+function updateSlide() {
+  if (activeSlide == 1) {
+    document.querySelector(".slide-1").classList.add("slide-visible");
+    document.querySelector(".slide-2").classList.remove("slide-visible");
+    activeSlide = 2;
+  } else {
+    document.querySelector(".slide-2").classList.add("slide-visible");
+    document.querySelector(".slide-1").classList.remove("slide-visible");
+    activeSlide = 1;
+  }
+}
