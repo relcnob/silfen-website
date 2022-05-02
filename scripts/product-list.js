bf7a0f96f6a18c68b70343f75e4c28c4d69a4570
Alessia Amore <a.amore9393@gmail.com>
2022-05-02 13:25:41 +0200

    js for product list done

---
+++ scripts/product-list.js
@@ -0,0 +1,31 @@
+const url2 =
+  "https://amorea.dk/WP/silfen/wp-json/wp/v2/product?per_page=4&?_embed";
+
+fetch(url2)
+  .then((res) => res.json())
+  .then((data) => handleProductList(data));
+
+function handleProductList(data) {
+  //function needs different name from api.js to work
+  //console.log(data);
+  data.forEach(showProductList);
+}
+
+function showProductList(product) {
+  console.log(product);
+  //grab template
+  const template = document.querySelector(".template-products").content;
+  //clone it
+  const myClone = template.cloneNode(true);
+  //change data
+  myClone.querySelector(".title-link").textContent = product.product_name;
+
+  myClone.querySelector(".pc-price").textContent = "DKK " + product.price;
+  myClone.querySelector(".pc-image a img").src = product.image.guid;
+  //remember to write down the right selector
+
+  //select parent
+  const parent = document.querySelector("main");
+  //append child
+  parent.appendChild(myClone);
+}
