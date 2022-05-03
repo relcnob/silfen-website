9c53d3702dd7dcfef6dee1f4a3d1895d7e152615
Fryderyk Boncler <f.relcnob@gmail.com>
2022-05-03 10:28:53 +0200

    added color fetching from WPpods

--- scripts/home-list.js
+++ scripts/home-list.js
@@ -16,6 +16,7 @@ function showHomeProductList(product) {
   //grab template
   const template = document.querySelector(".template-products").content;
 
+  let pcColors = product.color.split(" ");
   //clone it
   const myClone = template.cloneNode(true);
   //change data
@@ -31,6 +32,19 @@ function showHomeProductList(product) {
   myClone.querySelector(".pc-price").textContent = "DKK " + product.price;
   myClone.querySelector(".pc-image a img").src = product.image.guid;
   //remember to write down the right selector
+  // colors
+
+  console.log(pcColors);
+  pcColors.forEach((color) => {
+    const span = document.createElement("span");
+    span.textContent = " ";
+    span.setAttribute("class", "c-" + color + " pc-color");
+    console.log(span);
+    myClone.querySelector(".pc-colors").appendChild(span);
+  });
+  myClone
+    .querySelector(".pc-colors span:first-child")
+    .classList.add("c-selected");
 
   //select parent
   const parent = document.querySelector(".home-productlist");
